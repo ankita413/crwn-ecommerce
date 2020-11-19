9fb304605520c20a2ca4ba9cbb9b0a682b33d246
ankita413 <ankitamaurya413@gmail.com>
2020-11-19 19:36:56 +0530

    homepage file added

---
+++ crwn/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './homepage'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <Homepage/>
+    </div>
+  );
+}
+
+export default App;
